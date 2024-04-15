4999ffd65fbf94c004f2b0a0b7d7a0fd7da6f44a
br14nn <vituallabrian727@gmail.com>
2024-04-15 20:04:08 +0800

    updates

--- index.js
+++ index.js
@@ -10,14 +10,17 @@ app.use(express.static("views"));
 app.use(express.static("components"));
 
 app.get("/:place", (req, res) => {
+  // console.log(req.params.place);
+  setInterval(()=> {}, 1000)
   weather.find({ search: req.params.place, degreeType: "C" }, (err, result) => {
     if (err) {
       console.log(err);
     } else {
       let flag = "";
+      let weatherIcons = ["https://blob.weather.microsoft.com/static/weather4/en-us/law/30.gif","https://blob.weather.microsoft.com/static/weather4/en-us/law/29.gif", "https://blob.weather.microsoft.com/static/weather4/en-us/law/28.gif","https://blob.weather.microsoft.com/static/weather4/en-us/law/27.gif", "https://blob.weather.microsoft.com/static/weather4/en-us/law/26.gif"];
       let data = eval(JSON.stringify(result, null, 2));
 
-      console.log(data[0]);
+      console.log(data[0])
 
       if (data[0].location.name === "Davao, Philippines") {
         flag = "https://cdn-icons-png.flaticon.com/512/197/197561.png";
@@ -35,11 +38,18 @@ app.get("/:place", (req, res) => {
 
       res.render("index", {
         flag: flag,
+        windSpeed:data[0].current.winddisplay,
+        humidity: data[0].current.humidity,
         location: data[0].location.name,
         forecast: data[0].forecast,
+        weatherIcons: weatherIcons
       });
     }
   });
+});
+
+app.use((req, res) => {
+  res.status(404).redirect("/davao")
 });
 
 app.listen(port, () => {
